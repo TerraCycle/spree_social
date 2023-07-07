919a9eff43fe2d09a4a3d917a1c62dc86a3156ee
ivan-sheremeta <iisheremeta@gmail.com>
2023-07-07 10:17:15 +0300

    add fallback in routes in case undefined constant

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Spree::Core::Engine.add_routes do
              class_name: Spree::User,
              only: [:omniauth_callbacks],
              controllers: { omniauth_callbacks: 'spree/omniauth_callbacks' },
-             path: Spree::SocialConfig[:path_prefix]
+             path: defined?(Spree::SocialConfig) ? Spree::SocialConfig[:path_prefix] : 'users'
   scope '(:locale)', locale: /#{Spree.available_locales.join('|')}/, defaults: { locale: nil } do
     resources :user_authentications
 
